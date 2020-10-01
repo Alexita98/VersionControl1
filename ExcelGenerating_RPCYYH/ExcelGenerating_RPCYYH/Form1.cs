fb219bbdb54f40a176fb0a11fca7d2ede25872ef
Alexandra Szundy <alexandra.szundy@stud.uni-corvinus.hu>
2020-10-01 12:20:14 +0200

    Data Loading

--- ExcelGenerating_RPCYYH/ExcelGenerating_RPCYYH/Form1.cs
+++ ExcelGenerating_RPCYYH/ExcelGenerating_RPCYYH/Form1.cs
@@ -12,9 +12,28 @@ namespace ExcelGenerating_RPCYYH
 {
     public partial class Form1 : Form
     {
+        //A Form1 osztály szintjén hozz létre egy Flat típusú elemekből álló listára 
+        //mutató referenciát. (Nem kell inicializálni new-val.)
+        List<Flat> Flats;
+
+        //A Form1 osztály szintjén példányosítsd az ORM objektumot!
+        RealEstateEntities context = new RealEstateEntities();
+
         public Form1()
         {
             InitializeComponent();
+
+            //LoadData függvény meghívása
+            LoadData();
+            
+        }
+
+        //A létrehozott függvény célja kizárólag a program struktúrálása. 
+        //Enélkül az egész kód ömlesztve szerepelne a konstruktorban.
+        private void LoadData()
+        {
+            //A LoadData függvényen belül másold az adattáblát a memóriába!
+            Flats = context.Flats.ToList();
         }
     }
 }
