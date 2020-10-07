7221b60211b5c09761a848fddcc3e1af2170a36d
Alexandra Szundy <alexandra.szundy@stud.uni-corvinus.hu>
2020-10-07 09:01:02 +0200

    Data loading

--- VaR_week5/VaR_week5/Form1.cs
+++ VaR_week5/VaR_week5/Form1.cs
@@ -12,9 +12,24 @@ namespace VaR_week5
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        //Form1 osztály szintjén
+        //1) Tick típusú elemkből álló listára mutató referencia
+        List<Tick> Ticks;
+
+        //2) példányosítsd az ORM objektumot
+        PortfolioEntities context = new PortfolioEntities();
+        public Form1() //konstrukto
         {
             InitializeComponent();
+
+            //3) A konstruktorban másold az adattábát a memóriába
+            Ticks = context.Ticks.ToList();
+
+            //4) töltsd fel a lista elemeivel a DataGridView-t
+            dataGridView1.DataSource = Ticks;
+
+
+
         }
     }
 }
