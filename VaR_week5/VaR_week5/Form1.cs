4173ae8621a6b00cffcf945128c613cef4546be2
Alexandra Szundy <alexandra.szundy@stud.uni-corvinus.hu>
2020-10-07 10:20:10 +0200

    VaR calculation

--- VaR_week5/VaR_week5/Form1.cs
+++ VaR_week5/VaR_week5/Form1.cs
@@ -36,7 +36,24 @@ namespace VaR_week5
             //12) hívd meg a CreatePortfolio() függvényt
             CreatePortfolio();
 
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
 
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
 
         }
 
