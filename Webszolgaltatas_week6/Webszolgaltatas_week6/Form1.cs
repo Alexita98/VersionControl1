7c797602a2b595734fdae12d7197b3b12039b15a
Alexandra Szundy <alexandra.szundy@stud.uni-corvinus.hu>
2020-10-13 09:42:35 +0200

    Webszolgaltatas hivasa

--- Webszolgaltatas_week6/Webszolgaltatas_week6/Form1.cs
+++ Webszolgaltatas_week6/Webszolgaltatas_week6/Form1.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolgaltatas_week6.MnbServiceReference;
 
 namespace Webszolgaltatas_week6
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+
+            //1) Példányosítás (ehhez névtér behivatkozás)
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            //2) Példány létrehozás, paraméterek kitöltése
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020 - 01 - 01",
+                endDate = "2020-06-30"
+            };
+
+            //3) Hívd meg az mnbService GetExchangeRates nevű függvényét a request bemeneti
+            //paraméterrel, és a függvény visszatérési értékét tárold egy response nevű 
+            //változóba.
+            var response = mnbService.GetExchangeRates(request);
+
+            //4) A válaszból kérdezd le a GetExchangeRatesResult tulajdonság értékét egy 
+            //result változóba
+            var result = response.GetExchangeRatesResult;
+
+            //5) Entities mappa létrehozása a projektben
+            //6) A mappában hozz létre egy RateData nevű osztályt Rate, Currency, Value tulajdonságokkal
+
+
         }
     }
 }
