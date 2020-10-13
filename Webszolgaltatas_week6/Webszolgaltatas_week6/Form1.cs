abb6dc50d5c3dbc3efb281931129999b9500c227
Alexandra Szundy <alexandra.szundy@stud.uni-corvinus.hu>
2020-10-13 11:30:59 +0200

    Dinamikus funkciok

--- Webszolgaltatas_week6/Webszolgaltatas_week6/Form1.cs
+++ Webszolgaltatas_week6/Webszolgaltatas_week6/Form1.cs
@@ -24,21 +24,26 @@ namespace Webszolgaltatas_week6
         {
             InitializeComponent();
 
+            //23) Az eddig a konstruktorba írt kódot szervezd ki egy RefreshData nevű
+            //    függvénybe.
+            RefreshData();
+
             //0) Webszolgáltatás hívás függvény meghívása
             string result = webServiceCalling();
-         
 
             //5) Entities mappa létrehozása a projektben
             //6) A mappában hozz létre egy RateData nevű osztályt Rate, Currency, Value tulajdonságokkal
-
-            //8) Hozz létre egy DataGridView-t a Form1-en, és állítsd be, hogy a Rates legyen az adatforrása
-            dataGridView1.DataSource = Rates;
 
             //9) xml feldolgozás függvény meghívása
             xmlProcessing(result);
 
             //15) Adatvizualizációs függvény meghívása
             dataVisualization();
+
+            //22) Adj két DateTimePicker-t és egy üres ComboBox-ot a Form1-hez
+
+            //24) Rendelj eseménykezelőt a DateTimePicker-ek és a ComboBox alapértelmezett
+            //    eseményéhez (dupla klikk) is, és mindegyikből hívd meg a RefreshData-t.
 
         }
 
@@ -47,12 +52,16 @@ namespace Webszolgaltatas_week6
             //1) Példányosítás (ehhez névtér behivatkozás)
             var mnbService = new MNBArfolyamServiceSoapClient();
 
+            //25) A korábbi request létrehozást módosítsd úgy, hogy a dátumok a 
+            //    DateTimePicker-ek Value-jából, a valuta neve pedig a ComboBox 
+            //    SelectedItem -jéből érkezzenek. (Ne felejtsd el őket string-é konvertálni!)
+            //    A ComboBox Items listája a design nézetből kézzel is feltölthető néhány értékkel. Teszteléshez írd bele az “EUR” értéket (idézőjelek nélkül).
             //2) Példány létrehozás, paraméterek kitöltése
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020 - 01 - 01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
 
             //3) Hívd meg az mnbService GetExchangeRates nevű függvényét a request bemeneti
@@ -135,6 +144,30 @@ namespace Webszolgaltatas_week6
                 //Az Y tengely ne nullától induljon (ez egy bool tulajdonság)
                 ChartArea.AxisY.IsStartedFromZero = false;
 
+        }
+
+        private void RefreshData()
+        {
+            //24) ürítsd le a Rates lista tartalmát a Clear függvénnyel
+            Rates.Clear();
+
+            //8) Hozz létre egy DataGridView-t a Form1-en, és állítsd be, hogy a Rates legyen az adatforrása
+            dataGridView1.DataSource = Rates;
+        }
+
+        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void DateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
