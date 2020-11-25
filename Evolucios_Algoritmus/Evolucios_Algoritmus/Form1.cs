b87fd36a89ff79ed9db4ab35ceb43763c5a942cc
Alexandra Szundy <alexandra.szundy@stud.uni-corvinus.hu>
2020-11-25 17:28:48 +0100

    Iteracios lepes I

--- Evolucios_Algoritmus/Evolucios_Algoritmus/Form1.cs
+++ Evolucios_Algoritmus/Evolucios_Algoritmus/Form1.cs
@@ -16,6 +16,11 @@ namespace Evolucios_Algoritmus
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +29,24 @@ namespace Evolucios_Algoritmus
 
             /*gc.AddPlayer();
             gc.Start(true);*/
+
+            gc.GameOver += Gc_GameOver; ;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+
+         
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
         }
     }
 }
